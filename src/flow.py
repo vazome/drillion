f880fd797cc98b9e10ef75b5da6ab5fc28870cca
vazome <46573198+vazome@users.noreply.github.com>
2026-08-12 15:30:13 +0000

    Add initial implementations for various Python scripts

--- src/flow.py
+++
@@ -1,9 +0,0 @@
-l = "2 3 6 6 5"
-arr = map(int, l.split())  # promise to apply function, empties after single use.
-# [arr] promise wrapped into a list
-# [*arr] does the below list() logic.
-list_var = sorted(
-    set(arr)
-)  # list function that will execute the function int for each str object inside the map
-
-print(list_var[-2])
